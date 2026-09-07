dbd2a778c6aaeb71b312fc09e07cbcd81e7484bf
jehangeerk102-cyber-IT-Expert <jehangeerk102@gmail.com>
2026-09-07 16:10:14 +0500

    Delete TUMAR DETECTION1 directory

--- TUMAR DETECTION1/detector/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ModelStatusView, PredictView
-
-urlpatterns = [
-    path("predict/", PredictView.as_view(), name="predict"),
-    path("model-status/", ModelStatusView.as_view(), name="model-status"),
-]
